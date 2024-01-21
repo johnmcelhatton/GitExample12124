039f7406a6353836f375b4716b94fe0f12c3b29e
John McElhatton <jmcelhatton2010@gmail.com>
2024-01-21 13:56:35 -0500

    Initial Commit

---
+++ GitExample12124/GitExample12124App.swift
@@ -0,0 +1,17 @@
+//
+//  GitExample12124App.swift
+//  GitExample12124
+//
+//  Created by John McElhatton on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GitExample12124App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
